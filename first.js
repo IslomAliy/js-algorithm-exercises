d104e403ded759517d493bee1e9280402f166492
islomaliy <kislombek71@gmail.com>
2021-05-30 08:56:21 +0500

    first commit

---
+++ first.js
@@ -0,0 +1,5 @@
+// This algorithm exercises are taken from https://www.w3resource.com/javascript-exercises/ . Visit this web site for more algorithm exercises;
+
+/* 1. Write a JavaScript program to display the current day and time in the following format.  Go to the editor
+Sample Output : Today is : Tuesday.
+Current time is : 10 PM : 30 : 38 */
