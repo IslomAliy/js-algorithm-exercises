f10ea468f266b0c26c6ce238d87fe7584a1b1bb7
islomaliy <kislombek71@gmail.com>
2021-05-30 09:14:12 +0500

    solve basic

--- first.js
+++ first.js
@@ -3,3 +3,9 @@
 /* 1. Write a JavaScript program to display the current day and time in the following format.  Go to the editor
 Sample Output : Today is : Tuesday.
 Current time is : 10 PM : 30 : 38 */
+
+let days  = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+let today = new Date();
+let getDay = today.getDay();
+let getHours = today.getHours();
+console.log(`Today is ${days[getDay]}. And the time is ${getHours}`);
