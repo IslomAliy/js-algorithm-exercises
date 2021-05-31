5d31aae2cb4dd6a641546f80c83cb3355f594116
islomaliy <kislombek71@gmail.com>
2021-05-31 18:08:30 +0500

    second solved

--- second-points.js
+++ second-points.js
@@ -1,18 +1,21 @@
-checkspeed(82);
+checkspeed(81);
 
 var maxSpeed = 70;
 var count = 5;
-var points = 0;
+
 
 function checkspeed(speed){
+    let points = 0;
     if(speed - maxSpeed <= maxSpeed) {
         console.log('Okay');
     }
-    else if(speed - maxSpeed > maxSpeed ){
-        speed % count 
+    else if(speed - maxSpeed == true){
+        points += Math.round(speed % count );
     }
+    else if(points => 12) {console.log('Jarima!');}
+
+    else {console.log(speed);}
 }
 
-console.log('Sizga ${points}ta point berildi');
-
 // agar 70 dan oshiq bo'lsa maxSpeedni ayriymiz va 5 ga bo'lamiz
+
